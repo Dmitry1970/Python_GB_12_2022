d5e1163b02e8f52516f2ced840653f865ce7b371
Dmitry <badl1970@mail.ru>
2022-12-26 23:27:09 +0300

    homework4 task 1 is done

--- Python_seminars/homework3_3.py
+++ Python_seminars/homework3_3.py
@@ -14,13 +14,6 @@ for i in range(size):
     list.append(round(random.random()*10, 2))
 print(list)
 
-newList = []
-difference = 0
-for i in list:
-    newList.append(round(i * 100) % 100)
-print(newList)
-difference = (max(newList) - min(newList))/100
-print(difference)
 
 # 2-ой способ
 
@@ -30,12 +23,3 @@ for i in list:
     newList.append(round(i * 100) % 100)
 print(newList)
 
-max = 0
-min = 100
-for i in newList:
-    if max < i:
-        max = i
-    if min > i:
-        min = i
-difference = (max - min)/100
-print(difference)
